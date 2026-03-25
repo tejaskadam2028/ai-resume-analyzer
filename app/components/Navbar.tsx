627983ce1a38a927964d8e4fc04019d13fd541ab
AMOL GADE <kadamtejas254@gmail.com>
2026-03-25 15:55:26 +0530

    Added today's changes

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Navbar:() =>Element =() =>{
+
+    return(
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+
+            </Link>
+
+            <Link to="/upload" className="primary-button w-fit">
+                Upload Resume
+
+            </Link>
+
+
+
+
+        </nav>
+
+    )
+
+
+}
+export default Navbar
+
+
